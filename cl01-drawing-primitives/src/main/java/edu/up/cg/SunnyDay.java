f9c89ee6e2df1f675f4fcc9936f29b9bedb3119b
Alvaro Gallo <galloalvaro5@gmail.com>
2026-02-02 18:45:28 -0600

    reorganize and add cl01

--- cl01-drawing-primitives/src/main/java/edu/up/cg/SunnyDay.java
+++ cl01-drawing-primitives/src/main/java/edu/up/cg/SunnyDay.java
@@ -1,8 +1,10 @@
 package edu.up.cg;
+
 import java.awt.Color;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.function.Function;
 
 import javax.imageio.ImageIO;
 
@@ -15,7 +17,10 @@ public class SunnyDay {
       final int x = x1 + (int) ((x2 - x1) * t);
       final int y = y1 + (int) ((y2 - y1) * t);
 
-      img.setRGB(x, y, color.getRGB());
+      if (x >= 0 && x < img.getWidth() &&
+          y >= 0 && y < img.getHeight()) {
+        img.setRGB(x, y, color.getRGB());
+      }
     }
   }
 
@@ -27,19 +32,65 @@ public class SunnyDay {
     }
   }
 
+  public static void fill(BufferedImage img) {
+    for (int j = 0; j < img.getHeight(); j++) {
+      for (int i = 0; i < img.getWidth(); i++) {
+        img.setRGB(i, j, color.getRGB());
+      }
+    }
+  }
+
+  public static void fill_below_fn(BufferedImage img, Function<Double, Double> fn) {
+    int w = img.getWidth();
+    int h = img.getHeight();
+
+    for (int i = 0; i < w; i++) {
+      double x = i; // screen-space x
+      double y = fn.apply(x); // math-space height (0..1)
+
+      int sx = i;
+
+      int syFn = (int) (h - 1 - y * h * 0.9);
+      int syBase = h - 1;
+
+      draw_line(img, sx, syFn, sx, syBase, 1.0);
+    }
+  }
+
   public static void main(String[] args) {
-    int width = 400;
-    int height = 300;
+    int width = 1600;
+    int height = 1200;
     BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    color = Color.white;
+    fill(img);
     int center_sunx = (int) (width / 4);
     int center_suny = (int) (height / 4);
-    final int radius = 50;
+    final int radius = 150;
+
+    color = Color.red;
+    int large_offset = 90;
+    draw_line(img, center_sunx, center_suny - radius - large_offset, center_sunx, center_suny + radius + large_offset,
+        1.f);
+    draw_line(img, center_sunx - radius - large_offset, center_suny, center_sunx + radius + large_offset, center_suny,
+        1.f);
+
+    int little_offset = 45;
+    int start_little_line_x = (int) ((radius + little_offset) * Math.cos(Math.PI / 4));
+    int start_little_line_y = (int) ((radius + little_offset) * Math.sin(Math.PI / 4));
+    draw_line(img, center_sunx + start_little_line_x, center_suny + start_little_line_y,
+        center_sunx - start_little_line_x, center_suny - start_little_line_y,
+        1.f);
+
+    draw_line(img, center_sunx + start_little_line_x, center_suny - start_little_line_y,
+        center_sunx - start_little_line_x, center_suny + start_little_line_y,
+        1.f);
     color = Color.yellow;
     draw_circle_filled(img, radius, center_sunx, center_suny);
 
-    color = Color.white;
+    color = Color.green;
+    fill_below_fn(img, x -> 0.3 + 0.05 * Math.cos(x / 50));
     try {
-      File out = new File("sunny_day.png");
+      File out = new File("sunny_day.jpg");
       ImageIO.write(img, "JPG", out);
     } catch (Exception ex) {
       System.out.println(ex.getLocalizedMessage());
